e99d91d89a3586a0991e2e714c3b81b115f799e4
N1KX-x <xiaonikotianle@gmail.com>
2025-10-01 14:07:33 -0500

    studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,52 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+	while(true){
+		int redComponent = (int) (Math.random()*255);
+		int greenComponent = (int) (Math.random()*255);
+		int blueComponent = (int) (Math.random()*255);
+
+
+		double x = 0.5; //x1
+		double y = 0.25;//y1
+		double halfWidth = 0.5; //x2
+		double halfHeight = 0.25;///y2
+		StdDraw.clear(); // removing the previous frame
+		StdDraw.setPenColor(redComponent,greenComponent,blueComponent);
+		StdDraw.filledRectangle(x,y,halfWidth,halfHeight);
+
+		//4 rectangle
+		halfWidth = 0.5;
+		halfHeight = 0.05;
+		x = 0.5;
+		y = 0.25;
+		redComponent = (int) (Math.random()*255);
+		greenComponent = (int) (Math.random()*255);
+		blueComponent = (int) (Math.random()*255);
+		StdDraw.setPenColor(redComponent,greenComponent,blueComponent);
+		StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+
+		halfWidth = 0.5;
+		halfHeight = 0.05;
+		StdDraw.setPenColor(redComponent,greenComponent,blueComponent);
+		StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+
+
+		// circle
+		x = 0.25;
+		y = 0.25;
+		halfWidth = 0.1;
+		redComponent = (int) (Math.random()*255);
+		greenComponent = (int) (Math.random()*255);
+		blueComponent = (int) (Math.random()*255);
+		StdDraw.setPenColor(redComponent,greenComponent,blueComponent);
+		StdDraw.filledCircle(x,y,halfWidth);
+	StdDraw.pause(200);
+	StdDraw.show();
+	}
+
+
+
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,61 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String Shapename = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent =in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isfilled = in.nextBoolean();
+		double x = in.nextDouble(); //x1
+		double y = in.nextDouble();//y1
+		double halfWidth = in.nextDouble(); //x2
+		double halfHeight = in.nextDouble();///y2
+		double x3 = in.nextDouble();//x3
+		double y3 = in.nextDouble();//y3
 		
+		// drawing
+		// rectangle
+		StdDraw.clear(); // removing the previous frame
+		StdDraw.setPenColor(redComponent,greenComponent,blueComponent);
+		if(Shapename.equals("rectangle")){
+			if (isfilled == false){
+				StdDraw.rectangle(x,y,halfWidth,halfHeight);
+			}
+			else{
+				StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+			}
+		}
+
+		// ellipse
+		else if (Shapename.equals("ellipse")){
+			if (isfilled == false){
+				StdDraw.ellipse(x,y,halfWidth,halfHeight);
+
+			}
+			else{
+				StdDraw.filledEllipse(x,y,halfWidth,halfHeight);
+			}
+		}
+
+		// triangle
+		else if (Shapename.equals("triangle")){
+			double[] x_array = {x,halfWidth,x3};
+			double[] y_array = {y,halfHeight,y3};
+			if (isfilled == false){
+				StdDraw.polygon(x_array,y_array);
+			}
+			else{
+				StdDraw.filledPolygon(x_array,y_array);
+			}
+		}
+
+
+
+
+	StdDraw.show();
+		//Square
+
+
+
 	}
 }
